78448ac4af4a200b0c661a1d8b5aaccebcb4e197
Anwesh43 <anweshthecool0@gmail.com>
2021-01-06 05:03:02 +0530

    adding useDimension hook

--- src/hooks.js
+++ src/hooks.js
@@ -35,3 +35,35 @@ export const useDimension = () => {
         }
     })
 }
+
+const maxScale = (scale, i, n) => Math.max(0, scale - i / n)
+
+const divideScale = (scale, i, n) => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+const sinify = (scale) => Math.sin(scale * Math.PI)
+
+export const useStyle = ({w, h, n, scale}) => {
+    const sf = sinify(scale)   
+    const position = 'absolute'
+    const size = w / n
+    const top = `${h / 2 - size / 2}px`
+    const background = 'green'
+    return {
+        flipStyle(i) {
+            const left = `${i * size}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const sfi = divideScale(sf, 1, parts)
+            const WebkitTransform = `rotateY(${180 * sfi}deg)`
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height, 
+                background,
+                WebkitTransform
+            }
+        }
+    }
+}
