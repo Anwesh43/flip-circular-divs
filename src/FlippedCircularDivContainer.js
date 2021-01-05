2071525f7222928f16ab90342caeb60671f1ac1f
Anwesh43 <anweshthecool0@gmail.com>
2021-01-06 05:19:41 +0530

    adding FlippedCircularDivContainer

---
+++ src/FlippedCircularDivContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useAnimatedScale, useDimension} from './hooks'
+import FlippedCircularDiv from './FlippedCircularDiv'
+const FlipDivs = (n,w,h, scale) => {
+    const divs = []
+    for (var j = 0; j < n; j++) {
+        divs.push(<FlippedCircularDiv key = {`fdc_${j}`} n = {n} w = {w} h = {h} n = {n} scale = {scale} i = {j}/>)
+    }
+}
+
+const FlippedCircularDivContainer = ({n}) => {
+    const {w, h} = useDimension()
+    const {scale, start} = useAnimatedScale(n)
+    return <div>
+      <button onClick = {start}>Start</button>
+      {FlipDivs(n, w, h, scale)}
+    </div>
+}
+
+export default FlippedCircularDivContainer
+
